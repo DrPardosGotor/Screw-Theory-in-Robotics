829fa196d3f0345e8edce4aa84a091e785237afd
Dr. José M. Pardos-Gotor <dr.pardos.gotor@icloud.com>
2020-08-09 15:03:32 +0200

    +

---
+++ Exercises/E337_ST24R_FK_KUKAIIWA14_POE.m
@@ -0,0 +1,69 @@
+%% Screw Theory for Robotics - FORWARD Kinematics.
+% REDUNDANT & COLLABORATIVE robot - KUKA IIWA R820
+% Home "vertical" configuration.
+%
+% The goal of this exercise is to TEST:
+% Calculate the Homogeneous Matrix transformation for the end-effector of
+%
+% Mechanical characteristics of the Robot (AT REF HOME POSITION):
+% po = Origen for he STATIONARY system of reference.
+% pk = point in the crossing of the DOF Th1(rot) & Th2(rot) & Th3(rot).
+% pr = point in the axis of Th4(rot) Th5(rot).
+% pf = point in the crossing of the DOF Th5(rot), Th6(rot), Th7(rot).
+% pp = TcP Tool Center Point
+% hst0 = Tool (TcP) configuration (rot+tra) at robot reference position. 
+%
+%
+% Copyright (C) 2003-2020, by Dr. Jose M. Pardos-Gotor.
+%
+% This file is part of The ST24R "Screw Theory Toolbox for Robotics" MATLAB
+% 
+% ST24R is free software: you can redistribute it and/or modify
+% it under the terms of the GNU Lesser General Public License as published
+% by the Free Software Foundation, either version 3 of the License, or
+% (at your option) any later version.
+% 
+% ST24R is distributed in the hope that it will be useful,
+% but WITHOUT ANY WARRANTY; without even the implied warranty of
+% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+% GNU Lesser General Public License for more details.
+% 
+% You should have received a copy of the GNU Lesser General Public License
+% along with ST24R.  If not, see <http://www.gnu.org/licenses/>.
+%
+% http://www.
+%
+% CHANGES:
+% Revision 1.1  2019/02/11 00:00:01
+% General cleanup of code: help comments, see also, copyright
+% references, clarification of functions.
+%
+%% E337_ST24R_FK_KUKAIIWA14_POE
+%
+clear
+clc
+%
+Mag = zeros(1,7);
+for i = 1:7
+    Mag(i) = (rand-rand)*pi; % for testing various Theta1-Theta6
+end
+Mag
+%
+% Mechanical characteristics of the Robot:
+po=[0;0;0]; pk=[0;0;0.36]; pr=[0;0;0.78];
+pf=[0;0;1.18]; pp=[0.2;0;1.18];
+AxisX = [1 0 0]'; AxisY = [0 1 0]'; AxisZ = [0 0 1]'; 
+Point = [po pk po pr po pf pf];
+Joint = ['rot'; 'rot'; 'rot'; 'rot'; 'rot'; 'rot'; 'rot'];
+Axis = [AxisZ AxisY AxisZ -AxisY AxisZ -AxisY AxisX];
+Twist = zeros(6,7);
+for i = 1:7
+    Twist(:,i) = joint2twist(Axis(:,i), Point(:,i), Joint(i,:));
+end
+Hst0 = trvP2tform(pp)*rotY2tform(pi/2);
+%
+% STEP1: Apply ForwardKinemats to the Robot.
+TwMag = [Twist; Mag]; % assign the rand values to joint Theta magnitudes.
+HstR = ForwardKinematicsPOE(TwMag);
+noap = HstR * Hst0
+%

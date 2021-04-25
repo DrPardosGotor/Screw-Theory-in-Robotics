3ec25f06b77a9e29c996be23f260e87c57359f17
Dr. José M. Pardos-Gotor <dr.pardos.gotor@icloud.com>
2021-04-25 19:18:31 +0200

    Up

--- MATLAB_Functions/F092_PlotCARTERefVal.m
+++ MATLAB_Functions/F092_PlotCARTERefVal.m
@@ -31,52 +31,60 @@ x = REF(1,1:end);
 ax1 = subplot(6,1,1);
 y11 = REF(2,1:end)*100;
 y12 = VAL(2,1:end)*100;
-plot(ax1,x,y11,'y',x,y12,'r:','LineWidth',2);
+plot(ax1,x,y11,'b',x,y12,'r:','LineWidth',5);
 title(ax1,'Position X');
 ylabel(ax1,'cm');
-axis([0 x(1,end) -20 40]);
+set(gca,'FontSize',30);
+%axis([0 x(1,end) -20 40]);
 %
 ax2 = subplot(6,1,2);
 y21 = REF(3,1:end)*100;
 y22 = VAL(3,1:end)*100;
-plot(ax2,x,y21,'y',x,y22,'r:','LineWidth',2);
+plot(ax2,x,y21,'b',x,y22,'r:','LineWidth',5);
 title(ax2,'Position Y');
 ylabel(ax2,'cm');
-axis([0 x(1,end) -20 20]);
+set(gca,'FontSize',30);
+%axis([0 x(1,end) -20 20]);
 %
 ax3 = subplot(6,1,3);
 y31 = REF(4,1:end)*100;
 y32 = VAL(4,1:end)*100;
-plot(ax3,x,y31,'y',x,y32,'r:','LineWidth',2);
+plot(ax3,x,y31,'b',x,y32,'r:','LineWidth',5);
 title(ax3,'Position Z');
 ylabel(ax3,'cm');
-axis([0 x(1,end) 125 140]);
+set(gca,'FontSize',30);
+%axis([0 x(1,end) 125 140]);
 %
 ax4 = subplot(6,1,4);
 y41 = REF(5,1:end)*180/pi;
 y42 = VAL(5,1:end)*180/pi;
-plot(ax4,x,y41,'y',x,y42,'r:','LineWidth',2);
+plot(ax4,x,y41,'b',x,y42,'r:','LineWidth',5);
 title(ax4,'Orientation X');
 ylabel(ax4,'deg');
-axis([0 x(1,end) -25 20]);
+set(gca,'FontSize',30);
+%axis([0 x(1,end) 89 91]);
 %
 ax5 = subplot(6,1,5);
 y51 = REF(6,1:end)*180/pi;
 y52 = VAL(6,1:end)*180/pi;
-plot(ax5,x,y51,'y',x,y52,'r:','LineWidth',2);
+plot(ax5,x,y51,'b',x,y52,'r:','LineWidth',5);
 title(ax5,'Orientation Y');
 ylabel(ax5,'deg');
-axis([0 x(1,end) -20 40]);
+set(gca,'FontSize',30);
+%axis([0 x(1,end) -20 40]);
 %
 ax6 = subplot(6,1,6);
 y61 = REF(7,1:end)*180/pi;
 y62 = VAL(7,1:end)*180/pi;
-plot(ax6,x,y61,'y',x,y62,'r:','LineWidth',2);
+plot(ax6,x,y61,'b',x,y62,'r:','LineWidth',5);
 title(ax6,'Orientation Z');
 ylabel(ax6,'deg');
-xlabel(ax6,'Seconds');%
-axis([0 x(1,end) -200 200]);
+set(gca,'FontSize',30);
+%
+xlabel(ax6,'Seconds','FontSize',40);
+%axis([0 x(1,end) -200 200]);
 %
 fig1 = ax1;
+legend('TARGET Tool Trajectory','ACTUAL Tool Trajectory','FontSize',30)
 end
 %
